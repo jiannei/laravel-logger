2effb34e63df25e32f8462bda0012a3b7676f21e
Jiannei <Jiannei@users.noreply.github.com>
2020-11-30 07:38:05 +0000

    Apply fixes from StyleCI

--- src/Listeners/RequestArrivedListener.php
+++ src/Listeners/RequestArrivedListener.php
@@ -1,11 +1,18 @@
 <?php
 
-
+/*
+ * This file is part of the Jiannei/laravel-logger.
+ *
+ * (c) Jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Jiannei\Logger\Laravel\Listeners;
 
-use Jiannei\Logger\Laravel\Events\RequestArrivedEvent;
 use Illuminate\Support\Str;
+use Jiannei\Logger\Laravel\Events\RequestArrivedEvent;
 
 class RequestArrivedListener
 {
